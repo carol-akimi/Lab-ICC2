7434726c2bb149fb3c0ee2d03c9ba04abd950937
natalie <nataliecoelho@gmail.com>
2024-09-23 10:57:55 -0300

    codigo atualizado e diretório de testes

--- notaveis/alunos.c
+++ notaveis/alunos.c
@@ -1,5 +1,5 @@
 /*
-queria usar merge sort mas o shell tava pronto já então quis testar com ele (ainda noa funcionou)
+queria usar merge sort mas o shell tava pronto já então quis testar com ele
 */
 
 #include <stdio.h>
@@ -11,11 +11,11 @@ typedef struct aluno_{
     int aumento;
 }Aluno;
 
-Aluno* ler_alunos(FILE* fp);
+Aluno* ler_alunos(FILE* fp, int qtd);
 int contar_alunos(FILE* fp);
 void shell_sort(Aluno* lista, int n);
 void print_alunos(Aluno* lista, int k);
-void print_todos(Aluno* lista);     //temporaria
+void print_todos(Aluno* lista, int n);     //temporaria
 
 int main(void){
     char arquivo[20];
@@ -23,31 +23,32 @@ int main(void){
 
     scanf("%s %d", arquivo, &k);
     FILE* fp=fopen(arquivo, "r");
-
-    Aluno* alunos=ler_alunos(fp);
+    // char* s=malloc(200);
+    // fgets(s, sizeof(s), fp);
+    // free(s);
+    int qtd=contar_alunos(fp);
+    Aluno* alunos=ler_alunos(fp, qtd);
     fclose(fp);
 
-    shell_sort(alunos, sizeof(alunos)/sizeof(Aluno));
-    print_todos(alunos);    //testando a ordenação, tá errada
+    shell_sort(alunos, qtd);
+    //printf("\nprint todos:\n");
+    //print_todos(alunos, qtd);
+    //printf("\nprint alunos:\n");
     print_alunos(alunos, k);
     free(alunos);
     
     return 0;
 }
 
-Aluno* ler_alunos(FILE* fp){
+Aluno* ler_alunos(FILE* fp, int qtd){
     char nome[50];
     float n1, n3;
-    int qtd=contar_alunos(fp);
     Aluno* lista=malloc(qtd*sizeof(Aluno));
-
-    // if(lista==NULL)
-    //     printf("nao conseguiu alocar o vetor\n");
+    fseek(fp, 24, SEEK_SET);
 
     for(int i=0; i<qtd; i++){
-        fscanf(fp, "%[^,],%f,%*f,%f", nome, &n1, &n3);
+        fscanf(fp, " %[^,],%f,%*f,%f", nome, &n1, &n3);
         lista[i].nome=strdup(nome);
-        (lista[i].nome)[strlen(lista[i].nome)]='\0';    //arrumar
         lista[i].aumento=(int)(n3*10)-(int)(n1*10);
     }
 
@@ -59,42 +60,18 @@ int contar_alunos(FILE* fp){
     int n=0;
 
     while(fgets(s, sizeof(s), fp)!=NULL)
-        //printf("linha %d\n", n+1);
         n++;
     rewind(fp);
 
-    return n;
+    return n-1;
 }
 
-/*void inserir_aluno(Aluno* ini, Aluno* fim, Aluno aluno, int tam){
-    //  Insertion sort 
-        if(tam==1){
-
-        }
-        if(aluno.aumento<ini[tam/2].aumento)
-            inserir_aluno(ini+(tam/2), fim, aluno, tam/2);
-        else if(aluno.aumento>lista[tam/2].aumento)
-            inserir_aluno(ini, tam/2, aluno, tam/2);
-        else if(aluno.aumento==ini[tam/2].aumento){
-            j=tam/2;
-            while(strcmp(aluno.nome, ini[j].nome)<0){
-                j--;
-            }
-            for(int i=tam; i>j; i--)
-                ini[i]=ini[i-1];
-            ini[j]=aluno;
-        }
-    
-
-}*/
-
-/*  conferir se isso tá certo */
 void shell_sort(Aluno* lista, int n){
     for(int tam=n/2; tam>0; tam/=2){
         for(int i=tam; i<n; i++){
             Aluno temp=lista[i];
             int j;
-            for(j=i; j>=tam && (lista[j-tam].aumento>temp.aumento||(lista[j-tam].aumento==temp.aumento && strcmp(lista[j-tam].nome, temp.nome)<0)); j-=tam) //strcmp retorna número negativo se a primeira string for menor que a segunda quanto à ordem lexicográfica
+            for(j=i; j>=tam && (lista[j-tam].aumento<temp.aumento||(lista[j-tam].aumento==temp.aumento && strcmp(temp.nome, lista[j-tam].nome)<0)); j-=tam) //strcmp retorna número negativo se a primeira string for menor que a segunda quanto à ordem lexicográfica
                 lista[j]=lista[j-tam];
 
             lista[j]=temp;
@@ -103,11 +80,12 @@ void shell_sort(Aluno* lista, int n){
 }
 
 void print_alunos(Aluno* lista, int k){
-    for(int i=0; i<k||(lista[k].aumento==lista[i].aumento); i++)
-        printf("%s", lista[i].nome);
+    for(int i=0; i<k||(lista[k].aumento==lista[i+1].aumento); i++){
+        printf("%s\n", lista[i].nome);
+    }
 }
 
-void print_todos(Aluno* lista){
-    for(int i=0; i<3; i++)
+void print_todos(Aluno* lista, int n){
+    for(int i=0; i<n; i++)
         printf("%s", lista[i].nome);
 }
