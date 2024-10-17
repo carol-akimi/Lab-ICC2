eef690037e29f3c0e795cd7bfd24de8b6a90d0e3
carol-akimi <carolakimiueda@gmail.com>
2024-10-17 18:33:54 -0300

    stooge sort completo

--- radix_stooge/stooge.c
+++ radix_stooge/stooge.c
@@ -6,14 +6,13 @@
 typedef struct carta_{
     char* valor; /* String com naipe e valores. */
     /* Dígito que representa o valor na atual posição. Muda a cada etapa da ordenação. */
-    short int digito;
-    short int anterior; 
+    short int *valor_converte; 
 
 }Carta;
 
-short int converte(char* valores, int pos);
+short int* converte(char* valores, int n_digitos); 
 void stooge_full(Carta *baralho, int k, int n_digitos); 
-void stooge(Carta *baralho, int ini, int fim);
+void stooge(Carta *baralho, int ini, int fim, int pos);
 void print_array(Carta* baralho, int tam);
 
 int main(void){
@@ -27,14 +26,15 @@ int main(void){
 
     for(int i=0; i<k; i++){
         /* Aloca espaço para as strings do naipe e do valor da carta. */
-        char* naipe=(char*)malloc(5*sizeof(char));
+        char* naipe = (char*)malloc(5*sizeof(char));
         char* valor_carta = (char*)malloc(n_digitos*sizeof(char));
+
         /* Lê separadamente naipe e valor. */
         scanf(" %s %s", naipe, valor_carta);
         naipe[3]=' '; naipe[4]='\0'; /* Ajusta a string do naipe. */
         
         /* Aloca espaço para string unificada. */
-        baralho[i].valor=(char*)malloc(strlen(naipe)+strlen(valor_carta));
+        baralho[i].valor= (char*) malloc(strlen(naipe) + strlen(valor_carta));
         strcpy(baralho[i].valor, naipe); /* Copia cada uma para a principal. */
         strcat(baralho[i].valor, valor_carta);
 
@@ -43,7 +43,7 @@ int main(void){
         /* Libera a memória utilizada. */
     }
     /* Imprime a configuração inicial. */
-    
+
     stooge_full(baralho, k, n_digitos); /* Ordena. */
 
     /* Imprime o baralho depois do último passo da ordenação. */
@@ -62,82 +62,93 @@ int main(void){
 /* Função que transforma o valor de uma carta na determinada posição em um algarismo, 
 segundo a ordem especificada, para ser manipulado pelos algoritmos de ordenação. */
 
-short int converte(char* valores, int pos){
-    short int res; /* Dígito resultante da conversão. */
-    if(pos==0){ /* Se a posição é a primeira, deve-se converter o naipe. */
-        /* Copia da string de valor a parte do naipe. */
-        char naipe[4];
-        strncpy(naipe, valores, 3);
-        naipe[3]='\0';
-        /* Relaciona cada naipe a um valor segundo a ordem. */
-        if(strcmp(naipe, "♦") == 0)
-            res=0;
-        else if(strcmp(naipe, "♠") == 0)
-            res=1;
-        else if(strcmp(naipe, "♥") == 0)
-            res=2;
-        else if(strcmp(naipe, "♣") == 0)
-            res=3;
-    }else{ /* Se a posição não for zero, converte-se um valor em número. */
-        /* Acessa o caracter na posição desejada da string. */
-        char caracter =* (valores + pos + 3);
-        /* Designa um algarismo ao valor. */
-        switch(caracter){
-            case '4': res=0; break;
-            case '5': res=1; break;
-            case '6': res=2; break;
-            case '7': res=3; break;
-            case 'Q': res=4; break;
-            case 'J': res=5; break;
-            case 'K': res=6; break;
-            case 'A': res=7; break;
-            case '2': res=8; break;
-            case '3': res=9; break;
+short int* converte(char* valores, int n_digitos){
+    /* Um vetor de short ints é criado para guardar os dígitos do valor. */
+    short int *valor_sort=(short int*)malloc((n_digitos+1)*sizeof(short int));
+    /* Relaciona cada naipe a um valor segundo a ordem. */
+    char naipe[4];
+    strncpy(naipe, valores, 3);
+    naipe[3]='\0';
+    if(strcmp(naipe, "♦") == 0)
+        valor_sort[0] = 0;
+    else if(strcmp(naipe, "♠") == 0)
+        valor_sort[0] = 1;
+    else if(strcmp(naipe, "♥") == 0)
+        valor_sort[0] = 2;
+    else if(strcmp(naipe, "♣") == 0)
+        valor_sort[0] = 3;
+    /* A cada símbolo da string com o valor é atribuido um algarismo 
+    correspondente que mantém a ordem especificada no probolema. */
+    for (int i = 0; i <= n_digitos; i++){
+        switch(valores[i+3]){
+            case '4': valor_sort[i]=0; break;
+            case '5': valor_sort[i]=1; break;
+            case '6': valor_sort[i]=2; break;
+            case '7': valor_sort[i]=3; break;
+            case 'Q': valor_sort[i]=4; break;
+            case 'J': valor_sort[i]=5; break;
+            case 'K': valor_sort[i]=6; break;
+            case 'A': valor_sort[i]=7; break;
+            case '2': valor_sort[i]=8; break;
+            case '3': valor_sort[i]=9; break;
         }
     }
-    /* Retorna o dígito correspondente ao valor daquela posição. */
-    return res;
+    
+    return valor_sort; /* Retorna um vetor de short ints. */
 }
 
 void stooge_full(Carta *baralho, int k, int n_digitos){
-    for (int pos = n_digitos+1; pos > 0; pos--){
-        for (int i = 0; i < k; i++){    
-            baralho[i].digito = converte(baralho[i].valor, pos); 
-        }
-        stooge(baralho, 0, k-1); 
-    }
     for (int i = 0; i < k; i++){    
-        baralho[i].digito = converte(baralho[i].valor, 0); 
-        baralho[i].anterior = converte(baralho[i].valor, 1); 
-        baralho[i].digito = converte(baralho[i].valor, 0); 
+        baralho[i].valor_converte = converte(baralho[i].valor, n_digitos+4); 
     } 
-    stooge(baralho, 0, k-1); 
+
+    for (int w = 0; w < k; w++){
+        int a = 0; 
+        while (baralho[w].valor[a] != '\0'){
+            a++;
+
+        }
+    }
+
+    for (int j = n_digitos; j >= 0; j--){
+        stooge(baralho, 0, k-1, j); 
+    }
 }
 
 
-void stooge(Carta *baralho, int ini, int fim){
+void stooge(Carta *baralho, int ini, int fim, int pos){
+
+
     if (ini >= fim)
         return; 
 
-    if (baralho[ini].digito > baralho[fim].digito){
+    if (baralho[ini].valor_converte[pos] > baralho[fim].valor_converte[pos]){
         Carta aux; 
         aux = baralho[ini]; 
         baralho[ini] = baralho[fim]; 
         baralho[fim] = aux; 
     }
-    if (baralho[ini].digito == baralho[fim].digito){
-        if (baralho[ini].anterior > baralho[fim].anterior){
+    
+    if (baralho[ini].valor_converte[pos] == baralho[fim].valor_converte[pos]){
+        int p = pos + 1; 
+    
+        while (p >= 0 && baralho[ini].valor_converte[p] == baralho[fim].valor_converte[p]) {
+            p++;  // Continua verificando os valores anteriores enquanto forem iguais
+        }
+        // Se encontramos uma diferença em posições anteriores, trocamos
+        if (p >= 0 && baralho[ini].valor_converte[p] > baralho[fim].valor_converte[p]) {
             Carta aux; 
             aux = baralho[ini]; 
             baralho[ini] = baralho[fim]; 
             baralho[fim] = aux; 
         }
-    }    
+    } 
+
     if (fim - ini + 1 > 2){ //tem mais que dois elementos 
         int t = (fim - ini + 1)/3; 
-        stooge(baralho, ini, fim - t); 
-        stooge(baralho, ini + t, fim); 
-        stooge(baralho, ini, fim - t);
+        stooge(baralho, ini, fim - t, pos); 
+        stooge(baralho, ini + t, fim, pos); 
+        stooge(baralho, ini, fim - t, pos);
     }
     
 }
